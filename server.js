ab6376ed54f570864621d2891ab1762cf9fdaf6e
Kiko <ivanovkiko72@gmail.com>
2026-04-30 22:57:09 +0300

    add route + stops

--- server.js
+++ server.js
@@ -337,14 +337,16 @@ app.get("/liveTracking", async (req, res) => {
 
         lastKnownPositions[vehicleId].eta = eta
 
-       return res.json({
+      return res.json({
     vehicleId,
     lat,
     lon,
     eta,
     nextStop: tripData?.nextStop ?? null,
     delay: tripData?.delay ?? 0,
-    lineId: tripData?.trip?.lineId ?? ""
+    lineId: tripData?.trip?.lineId?.toString() || "",
+    stops: tripData?.trip?.stops || [],
+    shape: tripData?.trip?.shape || ""
 })
 
     } catch (e) {
